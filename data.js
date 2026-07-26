c4fc06d1d767420a09433d82f5a6acbd5e8fe14f
RamuMudhiraj <rammudhiraj2004@gmail.com>
2026-07-26 18:51:06 +0530

    Add vehicle data for 2 and 4 wheelers

---
+++ data.js
@@ -0,0 +1,84 @@
+const vehicleData = {
+
+    "2 Wheeler": {
+
+        "Hero": [
+            "Splendor Plus",
+            "HF Deluxe",
+            "Passion Plus",
+            "Glamour",
+            "Xpulse 200",
+            "Xtreme 160R"
+        ],
+
+        "Honda": [
+            "Shine",
+            "SP125",
+            "Unicorn",
+            "Hornet 2.0",
+            "Activa",
+            "Dio"
+        ],
+
+        "TVS": [
+            "Apache RTR 160",
+            "Apache RTR 200",
+            "Raider 125",
+            "Jupiter",
+            "Ntorq"
+        ],
+
+        "Yamaha": [
+            "R15",
+            "MT15",
+            "FZ-S",
+            "Fascino"
+        ],
+
+        "Royal Enfield":[
+            "Hunter 350",
+            "Classic 350",
+            "Bullet 350"
+        ]
+
+    },
+
+    "4 Wheeler": {
+
+        "Maruti Suzuki":[
+            "Swift",
+            "Baleno",
+            "Dzire",
+            "Brezza",
+            "Ertiga"
+        ],
+
+        "Hyundai":[
+            "i20",
+            "Creta",
+            "Venue",
+            "Verna"
+        ],
+
+        "Tata":[
+            "Punch",
+            "Nexon",
+            "Harrier",
+            "Safari"
+        ],
+
+        "Mahindra":[
+            "Thar",
+            "Scorpio N",
+            "XUV700"
+        ],
+
+        "Toyota":[
+            "Fortuner",
+            "Innova Hycross",
+            "Rumion"
+        ]
+
+    }
+
+};
